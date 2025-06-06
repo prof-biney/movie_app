46a0b9140c0a331e55834f35b9ca511cc8cf9b91
abiney1321 <abiney1321@gmail.com>
2025-06-06 00:58:33 +0000

    fix tailwindcss

--- components/Test.tsx
+++ components/Test.tsx
@@ -2,11 +2,7 @@ import { View, Text } from "react-native";
 import React from "react";
 
 const Test = () => {
-  return (
-    <View className="text-blue-500">
-      <Text className="text-blue-500">Test</Text>
-    </View>
-  );
+  return <Text className="text-green-500">Test</Text>;
 };
 
 export default Test;
