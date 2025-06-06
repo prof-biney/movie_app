60c6cc9478ef78d1de4a03ea5793a3294e0f5b75
abiney1321 <abiney1321@gmail.com>
2025-06-06 23:05:39 +0000

    set tailwind theme

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
 import Test from "@/components/Test";
-import { Text, View } from "react-native";
+import { StatusBar, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-red-500">Welcome</Text>
+      <Text className="text-5xl text-primary font-bold">Welcome</Text>
+      <StatusBar barStyle="dark-content" />
     </View>
   );
 }
