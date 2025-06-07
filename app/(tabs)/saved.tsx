2c2186ea67bbf3564f94c158e1324d582bdec792
abiney1321 <abiney1321@gmail.com>
2025-06-07 22:01:29 +0000

    work on layout

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Saved = () => {
   return (
-    <View>
-      <Text>Saved</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-primary font-bold">Saved</Text>
     </View>
   );
 };
